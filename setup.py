297188d589da2316ceea5d10f7fc546f3fbb606f
Tyson Clugg <tyson@clugg.net>
2015-05-27 15:19:38 +1000

    Bump version number, update CHANGES.rst.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-ddp',
-    version='0.6.4',
+    version='0.6.5',
     description=__doc__,
     long_description=open('README.rst').read(),
     author='Tyson Clugg',
