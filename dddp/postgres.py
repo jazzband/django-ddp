d03b259226397cf3cb901886ed0f7cc27968bcda
Tyson Clugg <tyson@clugg.net>
2015-09-18 17:46:20 +1000

    Allow Ctrl-C (Break) handling at any time, only run async DB connection when PostgresGreenlet is running.

--- dddp/postgres.py
+++ dddp/postgres.py
@@ -8,7 +8,6 @@ import gevent.queue
 import gevent.select
 import psycopg2  # green
 import psycopg2.extensions
-from geventwebsocket.logging import create_logger
 
 
 class PostgresGreenlet(gevent.Greenlet):
@@ -18,7 +17,8 @@ class PostgresGreenlet(gevent.Greenlet):
     def __init__(self, conn, debug=False):
         """Prepare async connection."""
         super(PostgresGreenlet, self).__init__()
-        self.logger = create_logger(__name__, debug=debug)
+        import logging
+        self.logger = logging.getLogger('dddp.postgres')
 
         # queues for processing incoming sub/unsub requests and processing
         self.connections = {}
@@ -28,37 +28,49 @@ class PostgresGreenlet(gevent.Greenlet):
         # connect to DB in async mode
         conn.allow_thread_sharing = True
         self.connection = conn
-        self.conn_params = conn.get_connection_params()
-        self.conn_params.update(
-            async=True,
-        )
-        self.conn = psycopg2.connect(**self.conn_params)
-        self.poll()  # wait for conneciton to start
-        self.cur = self.conn.cursor()
+        self.select_greenlet = None
 
     def _run(self):  # pylint: disable=method-hidden
         """Spawn sub tasks, wait for stop signal."""
-        gevent.spawn(self.process_conn)
-        self._stop_event.wait()
+        conn_params = self.connection.get_connection_params()
+        conn_params.update(
+            async=True,
+        )
+        conn = psycopg2.connect(**conn_params)
+        self.poll(conn)  # wait for conneciton to start
+        cur = conn.cursor()
+
+        import logging
+        logging.getLogger('dddp').info('=> Started PostgresGreenlet.')
+        cur.execute('LISTEN "ddp";')
+        while not self._stop_event.is_set():
+            try:
+                self.select_greenlet = gevent.spawn(
+                    gevent.select.select,
+                    [conn], [], [], timeout=None,
+                )
+                self.select_greenlet.join()
+            except gevent.GreenletExit:
+                self._stop_event.set()
+            finally:
+                self.select_greenlet = None
+            self.poll(conn)
+        cur.close()
+        conn.close()
 
     def stop(self):
         """Stop subtasks and let run() finish."""
         self._stop_event.set()
+        if self.select_greenlet is not None:
+            self.select_greenlet.kill()
 
-    def process_conn(self):
-        """Subtask to process NOTIFY async events from DB connection."""
-        self.cur.execute('LISTEN "ddp";')
-        while not self._stop_event.is_set():
-            gevent.select.select([self.conn], [], [], timeout=None)
-            self.poll()
-
-    def poll(self):
+    def poll(self, conn):
         """Poll DB socket and process async tasks."""
         while 1:
-            state = self.conn.poll()
+            state = conn.poll()
             if state == psycopg2.extensions.POLL_OK:
-                while self.conn.notifies:
-                    notify = self.conn.notifies.pop()
+                while conn.notifies:
+                    notify = conn.notifies.pop()
                     self.logger.info(
                         "Got NOTIFY (pid=%d, payload=%r)",
                         notify.pid, notify.payload,
@@ -90,17 +102,17 @@ class PostgresGreenlet(gevent.Greenlet):
                     tx_id = data.pop('_tx_id', None)
                     for connection_id in data.pop('_connection_ids'):
                         try:
-                            ws = self.connections[connection_id]
+                            websocket = self.connections[connection_id]
                         except KeyError:
                             continue  # connection not in this process
                         if connection_id == sender:
-                            ws.send(data, tx_id=tx_id)
+                            websocket.send(data, tx_id=tx_id)
                         else:
-                            ws.send(data)
+                            websocket.send(data)
                 break
             elif state == psycopg2.extensions.POLL_WRITE:
-                gevent.select.select([], [self.conn.fileno()], [])
+                gevent.select.select([], [conn.fileno()], [])
             elif state == psycopg2.extensions.POLL_READ:
-                gevent.select.select([self.conn.fileno()], [], [])
+                gevent.select.select([conn.fileno()], [], [])
             else:
                 self.logger.warn('POLL_ERR: %s', state)
