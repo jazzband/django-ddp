81824e5bfe42020df019c66261819eb06472bcd8
Tyson Clugg <tyson@clugg.net>
2015-09-22 17:00:03 +1000

    Fix dddp.test.test_project URL config and options for MiniMongo collections.

--- dddp/test/meteor_todos/meteor_todos.js
+++ dddp/test/meteor_todos/meteor_todos.js
@@ -1,8 +1,14 @@
 if (Meteor.isClient) {
   // This code only runs on the client
-  Django = DDP.connect('http://'+window.location.hostname+':8000/');
-  Tasks = new Mongo.Collection("django_todos.task", {"connection": Django});
-  Django.subscribe('Tasks');
+  options = {};
+  if (__meteor_runtime_config__.hasOwnProperty('DDP_DEFAULT_CONNECTION_URL')) {
+    Django = Meteor;
+  } else {
+    Django = DDP.connect(window.location.protocol + '//'+window.location.hostname+':8000/');
+    options.connection = Django;
+  }
+  Tasks = new Mongo.Collection("django_todos.task", options);
+  TaskSub = Django.subscribe('Tasks');
   Template.body.helpers({
     tasks: function () {
       return Tasks.find({});
