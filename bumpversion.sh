91c777321366d4d6872081aef79b8df36514959a
Tyson Clugg <tyson@clugg.net>
2015-10-14 01:05:46 +1100

    Update CHANGES.rst, bump version number with updated script.

--- bumpversion.sh
+++ bumpversion.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
+set -e
 cd "${0%/*}"
-cat setup.py | sed -e "s/version='[0123456789]\{1,\}\.[0123456789]\{1,\}\.[01234567989]\{1,\}'/version='$( git rev-parse --abbrev-ref HEAD | cut -d / -f 2 )'/" > setup.py
+OLD="$( python setup.py --version )"
+NEW="$( git rev-parse --abbrev-ref HEAD | cut -d / -f 2 )"
+echo "Bumping version ${OLD} -> ${NEW}...\n"
+sed -e "s/^    version='${OLD}'/    version='${NEW}'/" setup.py > .setup.py
+mv .setup.py setup.py
+sed -e "s/^version = '${OLD%.*}'/version = '${NEW%.*}'/" -e "s/^release = '${OLD}/release = '${NEW}'/" docs/conf.py > docs/.conf.py
+mv docs/.conf.py docs/conf.py
+git diff setup.py docs/conf.py
