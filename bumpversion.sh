8acdf40a1b41816fa1584bf4ed75ccf7f7d84bf9
Tyson Clugg <tyson@clugg.net>
2015-10-14 01:09:37 +1100

    Fix bug in new bumpversion script.

--- bumpversion.sh
+++ bumpversion.sh
@@ -6,6 +6,6 @@ NEW="$( git rev-parse --abbrev-ref HEAD | cut -d / -f 2 )"
 echo "Bumping version ${OLD} -> ${NEW}...\n"
 sed -e "s/^    version='${OLD}'/    version='${NEW}'/" setup.py > .setup.py
 mv .setup.py setup.py
-sed -e "s/^version = '${OLD%.*}'/version = '${NEW%.*}'/" -e "s/^release = '${OLD}/release = '${NEW}'/" docs/conf.py > docs/.conf.py
+sed -e "s/^version = '${OLD%.*}'/version = '${NEW%.*}'/" -e "s/^release = '${OLD}'/release = '${NEW}'/" docs/conf.py > docs/.conf.py
 mv docs/.conf.py docs/conf.py
 git diff setup.py docs/conf.py
