927311958d46c77d6c6ffa134a1c8babe4150bb3
Tyson Clugg <tyson@clugg.net>
2015-12-14 11:57:39 +1100

    Add `dddp.tests` test suite, just running doctests for now.

---
+++ dddp/tests.py
@@ -0,0 +1,31 @@
+"""Django DDP test suite."""
+
+import doctest
+import os
+import unittest
+import dddp.alea
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'dddp.test.test_project.settings'
+
+DOCTEST_MODULES = [
+    dddp.alea,
+]
+
+
+def load_tests(loader, tests, pattern):
+    """Specify which test cases to run."""
+    del pattern
+    suite = unittest.TestSuite()
+    # add all TestCase classes from this (current) module
+    for attr in globals().values():
+        try:
+            if not issubclass(attr, unittest.TestCase):
+                continue  # not subclass of TestCase
+        except TypeError:
+            continue  # not a class
+        tests = loader.loadTestsFromTestCase(attr)
+        suite.addTests(tests)
+    # add doctests defined in DOCTEST_MODULES
+    for doctest_module in DOCTEST_MODULES:
+        suite.addTest(doctest.DocTestSuite(doctest_module))
+    return suite
